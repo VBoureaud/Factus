5bd91318afd1859f00c0f8d6da7ebb934d3d54ec
VBoureaud <valentin.boureaud@epitech.eu>
2023-06-09 11:45:44 +0200

    Added ChakraUI

--- factus_front/src/App.tsx
+++ factus_front/src/App.tsx
@@ -1,25 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { ChakraProvider } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ChakraProvider>
+      <h1>Factus</h1>
+    </ChakraProvider>
   )
 }
 
