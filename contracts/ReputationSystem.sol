0bd1ec2e4ed95f96d05754c56ec4771f42e2af27
Peter <peter@interlay.io>
2023-06-10 22:22:00 +0200

    feat(contract): complete v1 of ReputationSystem smart contract

--- contracts/ReputationSystem.sol
+++ contracts/ReputationSystem.sol
@@ -1,40 +1,55 @@
+// SPDX-License-Identifier: cc0
 pragma solidity ^0.8.0;
 
 // TODO: optimize struct to take less storage
 struct Content {
     address creator;
-    int256 score; 
+    int256 score;
     uint256 unlockTimestamp;
     bool isProcessed; // true IF the content has been processed (author's reputation was updated), FALSE otherwise
 }
 
+struct ContentVote {
+    uint256 lockedAmount;
+    bool approve;
+}
+
 contract ReputationSystem {
+    uint256 LOCKED_AMOUNT_VOTING_WEI_DENOMINATOR = 10 ** 13;
+    mapping(string => address) public nostrKeyToAccount;
+    mapping(address => string) public accountToNostrKey;
 
-    mapping (string => address) public nostrKeyToAccount;
-    mapping (address => string) public accountToNostrKey;
+    mapping(address => int256) public accountReputation;
+    mapping(address => mapping(string => ContentVote)) public lockedVotes; // account -> nostr content id -> locked amount
 
-    mapping (address => int128) public accountReputation;
-    mapping (address => mapping (string => uint256)) public lockedAmounts; // account -> nostr content id -> locked amount
+    mapping(string => Content) public nostrIdToContent; // nostr content id -> content
 
-    mapping (string => Content) public nostrIdToContent; // nostr content id -> content
-
-    modifier hasNostrKeyRegistered  {
+    modifier hasNostrKeyRegistered() {
         bytes memory nostrKeyOfAccount = bytes(accountToNostrKey[msg.sender]);
-        require(nostrKeyOfAccount.length != 0, "Account did not register a nostr key yet");
+        require(
+            nostrKeyOfAccount.length != 0,
+            "Account did not register a nostr key yet"
+        );
         _;
     }
 
-    modifier isRegisteredContent (string memory nostrId) {
+    modifier isRegisteredContent(string memory nostrId) {
         Content memory content = nostrIdToContent[nostrId];
-        require(content.creator != address(0), "Content with this nostr id is not registered yet");
+        require(
+            content.creator != address(0),
+            "Content with this nostr id is not registered yet"
+        );
         _;
     }
 
-    // 
-    function registerNostrKey(string memory nostrKey) external  {
+    // Register nostr key with ethereum account
+    function registerNostrKey(string memory nostrKey) external {
         address ownerOfNostrKey = nostrKeyToAccount[nostrKey];
         bytes memory nostrKeyOfAccount = bytes(accountToNostrKey[msg.sender]);
-        require(nostrKeyOfAccount.length == 0, "Account already registered a key");
+        require(
+            nostrKeyOfAccount.length == 0,
+            "Account already registered a key"
+        );
         require(ownerOfNostrKey == address(0), "Nostr key already registered");
         nostrKeyToAccount[nostrKey] = msg.sender;
         accountToNostrKey[msg.sender] = nostrKey;
@@ -42,50 +57,118 @@ contract ReputationSystem {
 
     // Compute how much score will be added based on user's reputation
     // and amount they lock
-    function computeScoreVotingPower(address account, uint256 amount) public view returns (uint256) {
-        int128 reputation = accountReputation[account];
+    function computeScoreVotingPower(
+        address account,
+        uint256 amount
+    ) public view returns (uint256) {
+        int256 reputation = accountReputation[account];
         // TODO: handle this in more sophisticated way - e.g. with logarithmic fn
         // 1_000 = 1; 4 decimals
         // Minimum amount to lock is 0.00001 ether;
-        uint256 reputationWeiDenominator = 10**13;
+
         if (reputation < -1000 || amount < 0.00001 ether) {
             return 0;
         }
-        return uint(int(1000 + reputation)) * amount / reputationWeiDenominator / 1000 ;
+        return
+            (uint256(int256(1000 + reputation)) * amount) /
+            LOCKED_AMOUNT_VOTING_WEI_DENOMINATOR /
+            1000;
     }
 
-    function registerContent(string memory nostrContentId) external {
+    function registerContent(
+        string memory nostrContentId
+    ) external hasNostrKeyRegistered {
         Content memory content = nostrIdToContent[nostrContentId];
-        require(content.creator != address(0), "Content with this nostr id is registered already");
-        
-        uint256 unlockingTimestamp  = block.timestamp + 7 days;
-        Content memory newContentEntry = Content(msg.sender, 0, unlockingTimestamp, false);
+        require(
+            content.creator != address(0),
+            "Content with this nostr id is registered already"
+        );
+
+        uint256 unlockingTimestamp = block.timestamp + 7 days;
+        Content memory newContentEntry = Content(
+            msg.sender,
+            0,
+            unlockingTimestamp,
+            false
+        );
         nostrIdToContent[nostrContentId] = newContentEntry;
     }
 
-    function voteOnContent(string memory nostrContentId, bool approve) external payable isRegisteredContent(nostrContentId) {
+    // Locks user's tokens on voting
+    function voteOnContent(
+        string memory nostrContentId,
+        bool approve
+    )
+        external
+        payable
+        hasNostrKeyRegistered
+        isRegisteredContent(nostrContentId)
+        returns (uint256)
+    {
         // LOCK TOKENS AND ADD VOTES ON CONTENT
-        require(msg.value > 0.00001 ether, "Minimum amount to lock is 0.00001 eth.");
+        require(
+            msg.value > 0.00001 ether,
+            "Minimum amount to lock is 0.00001 eth."
+        );
+        require(
+            nostrIdToContent[nostrContentId].unlockTimestamp > block.timestamp,
+            "Voting period has ended already"
+        );
         // Register locked funds
-        lockedAmounts[msg.sender][nostrContentId] = msg.value;
+        lockedVotes[msg.sender][nostrContentId] = ContentVote(
+            msg.value,
+            approve
+        );
 
         // 0.00001 ETH = 1 content score point -> min amount
         uint256 scoreToAdd = computeScoreVotingPower(msg.sender, msg.value);
 
         // Increase or decrease based on
-        nostrIdToContent[nostrContentId].score += approve ? int(scoreToAdd) : (-1 * int(scoreToAdd));
+        nostrIdToContent[nostrContentId].score += approve
+            ? int(scoreToAdd)
+            : (-1 * int(scoreToAdd));
+
+        return scoreToAdd;
     }
 
     function processContentScore(string memory nostrContentId) public {
-        // CHECK: hasn't been processed
+        Content storage content = nostrIdToContent[nostrContentId];
+        if (content.isProcessed) {
+            return;
+        }
 
-        // handle points to author
+        accountReputation[content.creator] += content.score;
+        content.isProcessed = true;
     }
 
-    function unlockFunds(string memory nostrContentId) external isRegisteredContent(nostrContentId) {
-        // unlocks funds and increase/decrease reputation of account
-        // IF it's the first unlock for content then increase/decrease reputation of author 
+    function unlockFunds(
+        string memory nostrContentId
+    ) external isRegisteredContent(nostrContentId) {
+        Content storage content = nostrIdToContent[nostrContentId];
+        // Check that voting deadline has passed
+        require(
+            content.unlockTimestamp < block.timestamp,
+            "Cannot unlock staked amount yet"
+        );
+
+        // Unlock and overwrite storage to prevent reentrancy
+        uint lockedAmount = lockedVotes[msg.sender][nostrContentId]
+            .lockedAmount;
+        lockedVotes[msg.sender][nostrContentId].lockedAmount = 0;
+        require(lockedAmount > 0, "No locked amount found or already claimed");
+
+        // Pay outstanding locked funds
+        (bool sent, ) = msg.sender.call{value: lockedAmount}("");
+        require(sent);
+
+        // Modify user's reputation based on his voting
+        bool votedWithMajority = content.score >= 0 &&
+            lockedVotes[msg.sender][nostrContentId].approve;
+        accountReputation[msg.sender] += votedWithMajority
+            ? int(lockedAmount)
+            : (-1 * int(lockedAmount));
+
+        // Modify content author's reputation
+        processContentScore(nostrContentId);
     }
-
-
 }
